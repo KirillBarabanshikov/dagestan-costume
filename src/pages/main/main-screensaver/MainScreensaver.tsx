cfb6ed7be4bff7e604b58ba3a495ea5c866b394c
Kirill <kirillbar24@gmail.com>
2024-11-21 13:37:37 +0400

    added main screensaver

--- src/pages/main/main-screensaver/MainScreensaver.tsx
+++ src/pages/main/main-screensaver/MainScreensaver.tsx
@@ -1,3 +1,12 @@
+import styles from './MainScreensaver.module.scss';
+
 export const MainScreensaver = () => {
-    return <div>MainScreensaver</div>;
+    return (
+        <div className={styles.mainScreensaver}>
+            <video autoPlay loop muted playsInline>
+                <source src={'/video.mp4'} type='video/mp4' />
+            </video>
+            <h1>Примерим Дагестанский костюм?</h1>
+        </div>
+    );
 };
