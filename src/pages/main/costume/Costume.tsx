92222db2d514c3d93560d6e39b4c200ca2252c12
Kirill <kirillbar24@gmail.com>
2024-11-26 14:45:51 +0400

    added main costume

--- src/pages/main/costume/Costume.tsx
+++ src/pages/main/costume/Costume.tsx
@@ -1,3 +1,11 @@
+import styles from './Costume.module.scss';
+
 export const Costume = () => {
-    return <div>Costume</div>;
+    return (
+        <div className={styles.costume}>
+            <video autoPlay loop muted playsInline>
+                <source src={'/video.mp4'} type='video/mp4' />
+            </video>
+        </div>
+    );
 };
