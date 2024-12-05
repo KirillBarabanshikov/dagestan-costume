c64d1dadfdb8dfcaf7aa8aae963a2007d1e76463
Kirill <kirillbar24@gmail.com>
2024-12-05 17:24:10 +0400

    fix config.js

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
-window.API_URL = 'http://api.dagestan.costume.test.itlabs.top';
+window.API_URL = 'https://api.dagestan.costume.test.itlabs.top';
 window.SSE_URL = 'http://192.168.0.30:5003/.well-known/mercure?topic=state';
 window.JWT_TOKEN =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyJzaGl0Il19fQ.8tBVLOQD_s4cYLaEXdpezK5jP7mXsSdtGWkjb6Hn4R8';
