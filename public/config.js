14447dd145872596e8322f238d4b55d3c7a1f4ba
Kirill <kirillbar24@gmail.com>
2024-12-05 17:19:07 +0400

    fix config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.API_URL = 'http://api.dagestan.costume.test.itlabs.top';
-window.SSE_URL = 'http://192.168.0.30:5003//.well-known/mercure?topic=state';
+window.SSE_URL = 'http://192.168.0.30:5003/.well-known/mercure?topic=state';
 window.JWT_TOKEN =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyJzaGl0Il19fQ.8tBVLOQD_s4cYLaEXdpezK5jP7mXsSdtGWkjb6Hn4R8';
 window.PAYMENT_PASSWORD = '160371';
